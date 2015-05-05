4cd47199c6c02690a83733fde097fdb7d24e4cd4
identityclash <j3n@J3Ns-MacBook-Pro.local>
2015-05-06 02:19:55 +0800

    Added SqlBuilder to help in sql query creation; Added support for LPUSH and LINDEX
    Added SqlBuilder to help in sql query creation; Added support for LPUSH
    with multiple values and LINDEX; deleted other unnecessary functions.

--- test/test.js
+++ test/test.js
@@ -30,46 +30,34 @@ mappy.on('error', function (err) {
   console.log('Error from listener: ' + err.message);
 });
 
-setTimeout(function (err) {
+mappy.set('email', ['x', 'hello@blumr.com'], function (err) {
   if (err) {
-    console.log(err);
-  } else {
-
-    mappy.get('email', 'x', function (err, result) {
-      if (err) {
-        console.log('Error on get: ' + err);
-      } else {
-        console.log('x is initially ' + result);
-      }
-    });
-
-    mappy.set('email', ['x', 'honey@blumr.com'], function (err) {
-      if (err) {
-        console.log('Error on set: ' + err);
-      }
-    });
-
-    setTimeout(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        mappy.get('email', 'x', function (err, result) {
-          if (err) {
-            console.log('Error on get: ' + err);
-          } else {
-            console.log('x is finally ' + result);
-          }
-        });
-      }
-    }, 1000);
+    console.log('Error on set: ' + err);
   }
-}, 2000);
+});
 
-mappy.lpush('name', 'three three', function (err) {
+mappy.get('email', 'x', function (err, result) {
+  if (err) {
+    console.log('Error on get: ' + err);
+  } else {
+    console.log('x is finally ' + result);
+  }
+});
+
+mappy.lpush('name', ['four', 'five', 'six'], function (err) {
   if (err) {
     console.log('Error inserting in names: ' + err);
   }
 });
+
+mappy.lindex('name', -7, function (err, result) {
+  if (err) {
+    console.log('Error on lindex: ' + err);
+  } else {
+    console.log('lst:name is ' + result);
+  }
+});
+
 
 setTimeout(function (err) {
   if (err) {
